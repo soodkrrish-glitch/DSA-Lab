42677e3b2fc31ea61486adf3004ad7eaba15a6b9
soodkrrish-glitch <soodkrrish@gmail.com>
2025-08-21 15:05:32 +0530

    Create q5.cpp

---
+++ q5.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+    cout << "Enter number of rows: ";
+    cin >> r;
+    cout << "Enter number of columns: ";
+    cin >> c;
+
+    int a[20][20];
+
+    cout << "Enter elements of the matrix: ";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        int sum = 0;
+        for (int j = 0; j < c; j++) {
+            sum += a[i][j];
+        }
+        cout << "Sum of row " << i + 1 << " = " << sum << endl;
+    }
+
+    for (int j = 0; j < c; j++) {
+        int sum = 0;
+        for (int i = 0; i < r; i++) {
+            sum += a[i][j];
+        }
+        cout << "Sum of column " << j + 1 << " = " << sum << endl;
+    }
+
+    return 0;
+}
